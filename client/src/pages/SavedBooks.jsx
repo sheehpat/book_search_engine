d1adab3e886d02ba15773d6adc5f7f3b15012969
Patrick Sheehan <patrick@Patricks-MacBook-Air.local>
2024-06-09 19:48:45 -0600

    added to mutations/queries

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
 import {
   Container,
   Card,
@@ -7,12 +8,100 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+
+//import { getMe, deleteBook } from '../utils/API';
+import { REMOVE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
+  console.log("savedbooks checking the token!",token);
+  const [deleteIt] = useMutation(REMOVE_BOOK)
+  const { loading, error, data } = useQuery(GET_ME, {
+    variables: { token },
+  });
+
+console.log("savedbooks checking the error!", error);
+
+if (error) return <p>Error: {error.message}</p>;
+
+console.log("savedbooks checking the data!", data);
+  const userData = data?.me || []; 
+
+console.log(userData);
+
+  
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => { 
+    event.preventDefault
+    // const token = Auth.loggedIn() ? Auth.getToken() : null; 
+    console.log("savedbooks checking the bookId!", bookId);
+ console.log()
+    if (!token) {
+      return false;
+    }
+
+    try {
+    console.log("trying to delete", token);
+      const data = await deleteIt({
+        variables: { bookId, _id: token },
+      });
+
+     
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <div fluid className="text-light bg-dark p-5">
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </div>
+      <Container>
+        <h2 className='pt-5'>
+          {data.GetMe.bookCount
+            ? `Viewing ${data.GetMe.bookCount} saved ${data.GetMe.bookCount === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+        <Row>
+          {data.GetMe.savedBooks.map((book) => {
+            return (
+              <Col md="4">
+                <Card key={book.bookId} border='dark'>
+                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className='small'>Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                      Delete this Book!
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default SavedBooks;
+
+  /* const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
@@ -109,3 +198,4 @@ const SavedBooks = () => {
 };
 
 export default SavedBooks;
+ */
